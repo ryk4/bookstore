78afbb940ff9d77853c22df87e79305a5d958e5a
ryk4 <rytiskli@gmail.com>
2021-03-06 00:52:33 +0200

    fix css/js libraries not loaded over https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-
+        if(env('APP_ENV') !== 'local')
+        {
+            $url->forceSchema('https');
+        }
     }
 }
